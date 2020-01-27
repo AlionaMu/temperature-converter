7a8715a277ccf0bb750e528a33412092a1b960fb
AlionaMu <34584155+AlionaMu@users.noreply.github.com>
2020-01-27 17:09:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 
 
 exports.cToF = function (c) {
-
+   var f = 9/5 * tc + 32;
+    return f.toFixed(2);
 }
 
 exports.fToC = function (f) {
-
+    var c = 5/9 * (tf - 32);
+    return c.toFixed(2);
 }
